1e2418fa5b060bfae5f950ad8e6f800734f57c3e
Yernemm <yrnemmizer@gmail.com>
2020-06-17 00:20:24 +0100

    Initial commit

---
+++ Assets/GrapplingHook.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingHook : MonoBehaviour
+{
+
+    public LineRenderer lineRenderer;
+    private float ropeLength;
+    private Vector3 ropeEnd;
+
+    private bool attached = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       lineRenderer.SetPosition(0, transform.position);
+
+        if(Input.GetMouseButtonDown(1)){
+            //clicked
+
+            
+
+            RaycastHit hit;
+            var ray =  Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+            if(Physics.Raycast(ray, out hit)){
+                Debug.Log(hit.point);
+                lineRenderer.SetPosition(1, hit.point);
+
+                ropeLength = Vector3.Distance(transform.position, hit.point);
+                ropeEnd = hit.point;
+                lineRenderer.enabled = true;
+                attached = true;
+            }
+
+            
+
+
+        }else if(Input.GetMouseButton(1)){
+            //held
+
+           
+             
+
+        }else if(Input.GetMouseButtonUp(1)){
+            //let go
+
+            lineRenderer.enabled = false;
+            attached = false;
+        }
+        
+    }
+    
+    private void FixedUpdate() {
+        if(Input.GetMouseButton(1)){
+            if(Vector3.Distance(transform.position, ropeEnd) >= ropeLength && attached){
+
+                    gameObject.GetComponent<PlayerController>().velocity +=  Vector3.Normalize(ropeEnd - transform.position) * 5f;
+
+                }
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public CharacterController cc;
+    public float speed = 10f;
+
+    public float gravity = -90f;
+
+    public Vector3 velocity;
+    private bool isGrounded = false;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.1f;
+    public LayerMask groundMask;
+    public float jumpVelocity = 50f;
+
+    //public const float VELOCITY_DAMPENING_ADD = -1f;
+    public const float FIXED_VELOCITY_DAMPEN_MULTIPLIER = 0.97f;
+
+    public const float VELOCITY_MOVE_MULTIPLIER = 2f;
+
+    private const float FIXED_VELOCITY_DAMPEN_MULTIPLIER_MINIMUM_VELOCITY = 0.1f;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = new Vector3();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+        //cc.Move(move * Time.deltaTime);
+
+        
+
+        
+
+        //cc.Move(velocity * Time.deltaTime);
+
+        
+    }
+
+    private void FixedUpdate() {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -10f;
+        }
+
+
+        if(!isGrounded){
+            velocity.y += gravity * Time.deltaTime;
+        }else{
+            if(Input.GetKey("space")){
+                velocity.y = jumpVelocity;
+            }
+        }
+        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x * speed+ transform.forward * z * speed;
+
+        velocity.x = dampenVelocities(velocity.x);
+        velocity.z = dampenVelocities(velocity.z);
+
+
+        cc.Move((velocity + move) * Time.deltaTime);
+
+        velocity += move * VELOCITY_MOVE_MULTIPLIER * Time.deltaTime;
+        
+    }
+
+/*
+//old version using additive constant
+    float dampenVelocities(float velocity){
+        float velAfter = velocity + VELOCITY_DAMPENING_ADD * Time.deltaTime;
+        return  (velAfter < 0 ^ velocity < 0) ? 0 : velAfter;
+    }
+*/
+
+    float dampenVelocities(float velocity){
+        float velAfter = velocity * FIXED_VELOCITY_DAMPEN_MULTIPLIER;
+        return  Math.Abs(velAfter) < FIXED_VELOCITY_DAMPEN_MULTIPLIER_MINIMUM_VELOCITY ? 0 : velAfter;
+    }
+
+
+}
