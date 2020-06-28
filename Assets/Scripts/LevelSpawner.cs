5b695c1ddbf728867086b9aee370e8b2032d406a
Yernemm <yrnemmizer@gmail.com>
2020-06-28 22:21:27 +0100

    long

--- Assets/Scripts/LevelSpawner.cs
+++ Assets/Scripts/LevelSpawner.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class LevelSpawner : MonoBehaviour
 {
 
-    GameObject[] pieces;
+    public GameObject[] pieces;
+
+    System.Random random;
 
     // Start is called before the first frame update
     void Start()
     {
+        random = new System.Random();
+
+        for(int i = 0; i < 20; i ++){
+            int pieceId = random.Next(0, pieces.Length);
+            Instantiate(pieces[pieceId], new Vector3(100 * i, 0f, 0f), Quaternion.Euler(0f,0f,-90f));
+        }
         
     }
 
